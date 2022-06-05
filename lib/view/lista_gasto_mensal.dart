597b8912790cc4bb766da7f444c998e69940990f
WillianFarias <williansousafarias@gmail.com>
2022-06-05 19:35:18 -0300

    refactor

--- lib/view/lista_gasto_mensal.dart
+++ lib/view/lista_gasto_mensal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gastos_mensais/controller/gasto_controller.dart';
 import 'cadastro_gasto_mensal.dart';
+import 'package:gastos_mensais/model/gasto_mensal.dart';
+import 'gasto_item.dart';
 
 class ListaGastoMensal extends StatefulWidget {
   @override
@@ -17,6 +19,42 @@ class _ListaGastoMensalState extends State<ListaGastoMensal> {
         title: Text("\$ Gasto mensal \$"),
         backgroundColor: Colors.amber,
         centerTitle: true,
+      ),
+      body: FutureBuilder<List<GastoMensal>>(
+        initialData: [],
+        future: _gastoController.findAll(),
+// ignore: missing_return
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text("Carregando..."),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<GastoMensal> gastos = snapshot.data!;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final GastoMensal gastoMensal = gastos[index];
+                  return GastoItem(gastoMensal);
+                },
+                itemCount: gastos.length,
+              );
+              break;
+          }
+          return Text("Erro");
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
