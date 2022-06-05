99d8588a72e6acf2c05b827ddae81bfe856bb042
WillianFarias <williansousafarias@gmail.com>
2022-06-05 17:53:41 -0300

    metodo responsavel por inserir controller

---
+++ lib/controller/gasto_controller.dart
@@ -0,0 +1,19 @@
+import 'package:gastos_mensais/model/gasto_mensal.dart';
+import 'package:gastos_mensais/persistence/gasto_mensal_dao.dart';
+
+class GastoController {
+  Future<String> salvar(GastoMensal gastoMensal) async {
+    int res = 0;
+    try {
+      gastoMensal.id != null;
+//res = await GastoMensalDao.alterar(gastoMensal);
+    } catch (e) {
+      res = await GastoMensalDao.inserir(gastoMensal);
+    }
+    if (res == 0) {
+      return "Erro ao salvar registro";
+    } else {
+      return "Salvo com sucesso.";
+    }
+  }
+}
